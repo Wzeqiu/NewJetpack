d06eabec0ffd9b1a7696d0b86835a8aabeab83ae
wzeqiu <wangzeqiu@nineton.cn>
2025-03-26 19:52:44 +0800

    修改

--- common/src/main/java/com/common/kt/ViewKt.kt
+++ common/src/main/java/com/common/kt/ViewKt.kt
@@ -7,7 +7,7 @@ import com.common.common.R
 /**
  * view点击事件
  */
-inline fun <T : View> T.singleClick(interval: Long = 500, crossinline action: T.() -> Unit) {
+inline fun View.singleClick(interval: Long = 500, crossinline action: View.() -> Unit) {
     setOnClickListener {
         val currentTime = System.currentTimeMillis()
         val lastClickTime = getTag(R.id.view_check_interval_id)?.let { it as Long } ?: 0
@@ -20,7 +20,7 @@ inline fun <T : View> T.singleClick(interval: Long = 500, crossinline action: T.
 /**
  * view检查点击事件
  */
-inline fun <T : View> T.checkSingleClick(interval: Long = 500, crossinline action: T.() -> Unit) {
+inline fun View.checkSingleClick(interval: Long = 500, crossinline action: View.() -> Unit) {
     val currentTime = System.currentTimeMillis()
     val lastClickTime = getTag(R.id.view_check_interval_id)?.let { it as Long } ?: 0
     if (currentTime - lastClickTime < interval) return
