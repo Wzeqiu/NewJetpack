20f679629bbd92cdddaee75aa1d54094d09e9069
wzeqiu <wangzeqiu@nineton.cn>
2025-04-22 15:56:37 +0800

    feat(common): 新增 ActivityLauncher工具类
    用于在非 Activity 类中启动 Activity 并接收结果。使用该工具类可以简化在非 Activity 环境中启动 Activity 的过程，并且可以方便地处理返回结果。
    
    使用方法：
    1. 创建 ActivityLauncher 实例并保存在您的类中
    2.调用 launch 方法启动目标 Activity
    3. 在 callback 中处理返回结果
    
    该工具类实现了 LifecycleObserver，可以在 Activity 生命周期结束时自动清理资源，防止内存泄漏。

---
+++ common/src/main/java/com/common/utils/ActivityLauncher.kt
@@ -0,0 +1,74 @@
+package com.common.utils
+
+import android.content.Intent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * 用于在非Activity类中启动Activity并接收结果的工具类
+ * 使用方法:
+ * 1. 创建一个ActivityLauncher实例并保存在您的类中
+ * 2. 调用launch方法启动目标Activity
+ * 3. 在callback中处理返回结果
+ */
+class ActivityLauncher(private val activity: FragmentActivity) : DefaultLifecycleObserver {
+    companion object {
+        /**
+         * 自动计数防止code 冲突
+         */
+        private val mNextLocalRequestCode: AtomicInteger by lazy { AtomicInteger() }
+    }
+
+    // ActivityResultLauncher用于启动Activity并接收结果
+    private var launcher: ActivityResultLauncher<Intent>? = null
+
+    // 用于存储回调函数
+    private var resultCallback: ((ActivityResult) -> Unit)? = null
+
+    init {
+        // 在构造函数中注册ActivityResultLauncher
+        launcher = activity.activityResultRegistry.register(
+            "customize_activity_rq#${mNextLocalRequestCode.getAndIncrement()}",
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            launcher?.unregister()
+            // 当Activity返回结果时，调用保存的回调函数
+            resultCallback?.invoke(result)
+            // 调用后清空回调，防止内存泄漏
+            resultCallback = null
+            launcher = null
+            activity.lifecycle.removeObserver(this)
+        }
+
+        activity.lifecycle.addObserver(this)
+    }
+
+    /**
+     * 启动Activity并接收结果
+     * @param context 上下文
+     * @param intent 要启动的Intent
+     * @param callback 接收结果的回调函数
+     */
+    fun launch(intent: Intent, callback: (ActivityResult) -> Unit) {
+        // 保存回调函数
+        resultCallback = callback
+        // 启动Activity
+        launcher?.launch(intent)
+    }
+
+    /**
+     * 当LifecycleOwner销毁时，清理资源
+     */
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        launcher?.unregister()
+        resultCallback = null
+        launcher = null
+        activity.lifecycle.removeObserver(this)
+    }
+} 
