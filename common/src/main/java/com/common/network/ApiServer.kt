160271e4054bf0d63819da2133eefef6ea05fd28
zeqiu.wang <zeqiu.wang@163.com>
2025-07-04 21:45:02 +0800

    refactor(network): 重构网络请求并优化项目配置
    - 更新 ApiServer 接口，使用 suspend 关键字和 @Query 注解- 删除 BaseViewModel 类，简化项目结构
    - 更新 gradle 属性，启用 parallel 构建和配置缓存
    - 修复 AGP 版本号
    - 添加阿里云 Maven 仓库配置

--- common/src/main/java/com/common/network/ApiServer.kt
+++ common/src/main/java/com/common/network/ApiServer.kt
@@ -2,8 +2,9 @@ package com.common.network
 
 import com.common.db.dao.AITaskInfo
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiServer {
     @POST("v5/aigc-task/create.html")
-    fun getTaskInfo(taskId: String): BaseResponse<AITaskInfo>
+    suspend fun getTaskInfo(@Query("taskId") taskId: String): BaseResponse<AITaskInfo>
 }
